360ef9fa01846bee83159db03a82483aa6e61567
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2021-10-18 13:33:48 +0900

    新規csv代入

--- memo.rb
+++ memo.rb
@@ -8,15 +8,16 @@ memo_type = memo_type.to_i
 
 if memo_type  == 1 
   puts "拡張子を除いたファイルを入力して下さい"  #条件がtrueの場合実行される
-  memo = gets.to_s
-  CSV.open("#{memo}.csv", "w")
+  memo = gets.chomp
+  File.open("#{memo}.csv", "w")
 
   puts "”メモしたい内容を記入してください”"  #条件がtrueの場合実行される
   puts "”完了したらCtrl ＋　Dをおします。”"
   
-
-  memo_text = gets
-
+  message = gets.chomp
+  File.open("#{memo}.csv","a"){|f|
+  f.write("#{message}")
+  }
   if memo_text == "コントロール＋D"
     puts "解除"  #条件がtrueの場合実行される
   else 
@@ -25,8 +26,8 @@ if memo_type  == 1
 
 elsif memo_type  == 2
     puts "拡張子を除いたファイルを入力して下さい2"  #条件がtrueの場合実行される
-    memo = gets.to_s
-    CSV.open("#{memo}.csv")
+    memo = gets.chomp
+    File.open("#{memo}.csv")
     
     
     puts "”メモしたい内容を記入してください”"  #条件がtrueの場合実行される
@@ -37,10 +38,9 @@ elsif memo_type  == 2
     puts "解除”"  #条件がtrueの場合実行される
   else 
       puts "for文で永遠に戻しつつcsvへ"  #条件がfalseの場合実行される
-      CSV.open('test.csv','a') do |test|
-        test << ["A","B","C"]
-        test << ["milk","coffee","water"]
-       end
+      memo_message = gets
+      File.put "#{memo_message}"
+      
   end
 
 else
