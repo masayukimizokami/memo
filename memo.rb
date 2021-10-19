4558e06ca8db7985ccd978c9fb883a56a2b87012
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2021-10-19 15:27:40 +0900

    繰り返し

--- memo.rb
+++ memo.rb
@@ -2,8 +2,7 @@ require "csv"
 puts "1(新規でメモを作成)2(既存のメモ編集する)"
 #memo_type = gets.to_s
 
-memo_type = gets.to_s
-memo_type = memo_type.to_i
+memo_type = gets.to_i
 
 if memo_type  == 1 
   puts "拡張子を除いたファイルを入力して下さい"  
@@ -13,10 +12,12 @@ if memo_type  == 1
   puts "”メモしたい内容を記入してください”" 
   puts "”完了したらCtrl ＋　Dをおします。”"
   
-  message = gets.chomp
-  File.open("#{memo}.csv","a"){|f|
-  f.write("#{message}")
-  }
+  while 1 == 1 do
+    message = gets.to_s
+    File.open("#{memo}.csv","a"){|f|
+    f.write("#{message}")
+    }
+  end
 
 elsif memo_type  == 2
     puts "編集したいファイルを入力して下さい"  
@@ -25,15 +26,17 @@ elsif memo_type  == 2
     
     puts "”編集したい内容を記入してください”"  
     puts "”完了したらCtrl ＋　Dをおします。”"
-    message = gets.chomp
+     
+   while message != element.send_keys(:control,'d') do
+    message = gets.to_s
     File.open("#{memo}.csv","a"){|f|
     f.write("#{message}")
     }
-
+   end 
 else
     puts "１か２を記入してください"  #条件がfalseの場合実行される
 end
 
-
+ 
 
 
