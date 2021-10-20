9e1cebaee8f45aca3073ce0c76d4a2e7c7d33ca3
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2021-10-21 05:25:44 +0900

    ctrl＋dのエラー改善

--- memo.rb
+++ memo.rb
@@ -6,38 +6,37 @@ memo_type = gets.to_i
 
 if memo_type  == 1 
   puts "拡張子を除いたファイルを入力して下さい"  
-  memo = gets.chomp
-  File.open("#{memo}.csv", "w")
-
+  
+  file = gets.chomp
+  
   puts "”メモしたい内容を記入してください”" 
   puts "”完了したらCtrl ＋　Dをおします。”"
   
-  attrs = Array.new
-   while line = $stdin.gets
-    attrs << line.chomp.split
-   end
+  memo_type = STDIN.read
+
+    CSV.open("#{file}.csv","w") do |memo|
+
+        memo << ["#{memo_type}"]
+    end
 
 elsif memo_type  == 2
-    puts "編集したいファイルを入力して下さい"  
-    memo = gets.chomp
-    File.open("#{memo}.csv")
+  puts "拡張子を除いたファイルを入力して下さい"  
+  file = gets.chomp
     
     puts "”編集したい内容を記入してください”"  
     puts "”完了したらCtrl ＋　Dをおします。”"
    
-        attrs = Array.new
-        while line = $stdin.gets
-         attrs << line.chomp.split
-        end
+    memo_type = STDIN.read
+
+    CSV.open("#{file}.csv","a") do |memo|
+
+        memo << ["#{memo_type}"]
+    end
     
 else
     puts "１か２を記入してください"  #条件がfalseの場合実行される
 end
 
-next_text = true
 
-while next_text
-  next_text = re_text
-end
 
 
